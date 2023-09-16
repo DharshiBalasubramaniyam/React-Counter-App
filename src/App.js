87e39cdd7fcf2fa70dae5f0f8d96db43282be64c
Dharshib <Dharshib.8@gmail.com>
2023-09-16 14:59:48 +0530

    add increase count function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count+1);
+  }
+
+
+
   return (
     <section>
       <div className='container'>
         <h1>Counter App</h1>
-        <h2>12</h2>
+        <h2>{count}</h2>
         <div classname='btns'>
-          <button>Decrease</button>
-          <button>Increase</button>
+          <button>
+            Decrease
+          </button>
+          <button
+             onClick={increaseCount}
+          >
+            Increase
+          </button>
         </div>
       </div>
     </section>
