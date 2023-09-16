adb3eb411ed60d9e88ff50682f249153ea37c0fc
Dharshib <Dharshib.8@gmail.com>
2023-09-16 14:27:08 +0530

    add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <section>
-      <div classname='container'>
+      <div className='container'>
         <h1>Counter App</h1>
         <h2>12</h2>
         <div classname='btns'>
